ca085d0ac7c8d1617cb2df4bd2cb308ef5154133
Nazar Lytvynenko <nazar3lv@gmail.com>
2022-11-14 14:02:16 +0200

    update HW6

--- Homework 6/index.js
+++ Homework 6/index.js
@@ -120,12 +120,14 @@ Selector text 2: Third paragraph */
 
       // let main = document.createElement("main");
       // main.className = "mainClass check item";
-      // document.body.append(main);
 
       // let div = document.createElement("div");
       // div.id = "myDiv";
-      // main.prepend(div);
 
       // let par = document.createElement("p");
       // par.innerHTML = "First paragraph";
+
+      // document.body.append(main);
+      // main.prepend(div);
       // div.prepend(par);
+      
