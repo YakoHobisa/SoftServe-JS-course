e7d7860ac2fcef26d50a977c1e79de1cfa05b54b
Nazar Lytvynenko <nazar3lv@gmail.com>
2022-11-19 17:35:26 +0200

    updated HW 7

--- Homework 7/index.js
+++ Homework 7/index.js
@@ -4,13 +4,13 @@
 	3) із затримкою 2 сек перемістіть вікно в точку з координатами (200, 200)
 	4) із затримкою 2 сек закрийте вікно. */
 
-let newWin;
-    function newWindow() {
-        newWin = window.open("", "", "width=300, height=300");
-    };
-    setTimeout(function(){newWin.resizeTo(500, 500)}, 2000);
-    setTimeout(function(){newWin.moveTo(200, 200)}, 4000);
-    setTimeout(function(){newWin.close()}, 6000);
+// let newWin;
+//     function newWindow() {
+//         newWin = window.open("", "", "width=300, height=300");
+//     };
+//     setTimeout(function(){newWin.resizeTo(500, 500)}, 2000);
+//     setTimeout(function(){newWin.moveTo(200, 200)}, 4000);
+//     setTimeout(function(){newWin.close()}, 6000);
 
 /* 2. Для заданої HTML-сторінки:
 
@@ -22,13 +22,13 @@ let newWin;
 напишіть функцію changeCSS(), яка спрацьовуватиме по кліку на кнопку і змінюватиме стиль вмісту тега <p>:
 колір шрифту – оранжевий, розмір шрифту 20 пс, шрифт сімейства "Comic Sans MS". */
 
-let par = document.querySelector("#text");
+// let par = document.querySelector("#text");
 
-function changeCSS() {
-    par.style.color = "orange";
-    par.style.fontSize = "20px";
-    par.style.fontFamily = "Comic Sans MS";
-}
+// function changeCSS() {
+//     par.style.color = "orange";
+//     par.style.fontSize = "20px";
+//     par.style.fontFamily = "Comic Sans MS";
+// }
 
 /* 3. Задано сторінку з 3 кнопками і 1 лінкою. Напишіть Javascript код і реалізуйте HTML-сторінку 
 з відповідними подіями на кожному елементові:
@@ -38,33 +38,33 @@ function changeCSS() {
 	4) При наведенні на лінку – колір фону стає жовтим, при відведенні – білим.
 	Приклад – курсор наведений на лінку. */
  
-    document.querySelector("#backgrClr").onclick = function() {
-        document.body.style.backgroundColor = "blue";
-     };
-     document.querySelector("#backgrClrDoubleClick").ondblclick = function() {
-        document.body.style.backgroundColor = "pink";
-     };
-     document.querySelector("#backgrClrHoldRelease").onmousedown = function() {
-        document.body.style.backgroundColor = "purple";
-     };
-     document.querySelector("#backgrClrHoldRelease").onmouseup = function() {
-        document.body.style.backgroundColor = "white";
-     };
-     document.querySelector("#backgrClrOver").onmouseover = function() {
-        document.body.style.backgroundColor = "yellow";
-     };
-     document.querySelector("#backgrClrOver").onmouseout = function() {
-        document.body.style.backgroundColor = "white";
-     };
+   //  document.querySelector("#backgrClr").onclick = function() {
+   //      document.body.style.backgroundColor = "blue";
+   //   };
+   //   document.querySelector("#backgrClrDoubleClick").ondblclick = function() {
+   //      document.body.style.backgroundColor = "pink";
+   //   };
+   //   document.querySelector("#backgrClrHoldRelease").onmousedown = function() {
+   //      document.body.style.backgroundColor = "purple";
+   //   };
+   //   document.querySelector("#backgrClrHoldRelease").onmouseup = function() {
+   //      document.body.style.backgroundColor = "white";
+   //   };
+   //   document.querySelector("#backgrClrOver").onmouseover = function() {
+   //      document.body.style.backgroundColor = "yellow";
+   //   };
+   //   document.querySelector("#backgrClrOver").onmouseout = function() {
+   //      document.body.style.backgroundColor = "white";
+   //   };
 
 
 /* 4. Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку.
 Можуть видалятися всі елементи в будь-якому порядку. */
 
- let carsElement = document.querySelector("#cars")
-    document.querySelector("#deleteSelected").onclick = function() {
-        carsElement.remove(element.selectedIndex);
-    };
+//  let carsElement = document.querySelector("#cars")
+//     document.querySelector("#deleteSelected").onclick = function() {
+//         carsElement.remove(element.selectedIndex);
+//     };
 
 
 /* 5. Реалізуйте програму, яка по натисканню на кнопку виводитиме повідомлення 
@@ -73,31 +73,31 @@ function changeCSS() {
 
 Приклад роботи: */
 
-     document.querySelector("#liveBttn").onclick = function() {
-        document.querySelector("#clickMess").innerText += "I was pressed! \r\n";
-     };
-     document.querySelector("#liveBttn").onmouseover = function() {
-        document.querySelector("#clickMess").innerText += "Mouse on me! \r\n";
-     };
-     document.querySelector("#liveBttn").onmouseout = function() {
-        document.querySelector("#clickMess").innerText += "Mouse is not on me! \r\n";
-     };
+   //   document.querySelector("#liveBttn").onclick = function() {
+   //      document.querySelector("#clickMess").innerText += "I was pressed! \r\n";
+   //   };
+   //   document.querySelector("#liveBttn").onmouseover = function() {
+   //      document.querySelector("#clickMess").innerText += "Mouse on me! \r\n";
+   //   };
+   //   document.querySelector("#liveBttn").onmouseout = function() {
+   //      document.querySelector("#clickMess").innerText += "Mouse is not on me! \r\n";
+   //   };
 
 
 /* 6. Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) вікна браузера
 і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення. */
 
  
-window.onresize = displayWindowSize;
-window.onload = displayWindowSize;
-let myWidth;
-let myHeight;
+// window.onresize = displayWindowSize;
+// window.onload = displayWindowSize;
+// let myWidth;
+// let myHeight;
 
-function displayWindowSize() {
-    myWidth = window.innerWidth;
-    myHeight = window.innerHeight;
-    document.querySelector("#dimensions").innerHTML = "Width: " + myWidth + ", Height: " + myHeight;
-};
+// function displayWindowSize() {
+//     myWidth = window.innerWidth;
+//     myHeight = window.innerHeight;
+//     document.querySelector("#dimensions").innerHTML = "Width: " + myWidth + ", Height: " + myHeight;
+// };
 
 
 /* 7*. На сторінці потрібно реалізувати 2 випадаючих списки. У першому містяться назви країн,
@@ -120,19 +120,72 @@ function displayWindowSize() {
 // ----------not done yet--------- 
 
 
-// let mealsByCategory = {
-//     A: [],
-//     B: [],
-//     C: []
+// let cities = {
+//     Germany: ["Berlin", "Hamburg", "Munich", "Cologne", "Stuttgart"],
+//     USA: ["New York", "Los Angeles", "Chicago", "Houston", "San Francisco"],
+//     Ukraine: ["Kyiv", "Lviv", "Vinnytsia", "Ternopil", "Ivano-Frankivsk"]
+// };
+// const countrySel = document.querySelector("#country");
+// const citiesSel = document.querySelector("#cities");
+// const p = document.querySelector("#displayCountryCity");
+
+
+// countrySel.addEventListener("change", e => {
+//    p.innerText = e.target.value;
+//    if (e.target.value == "Germany") {
+//        removeAll();
+//        for (let i = 0; i < cities.length; i++) {
+//            citiesSel.options[citiesSel.options.length] = new Option(cities[0][i], cities[0][i]);
+//        }
+//    } else if(e.target.value == "USA") {
+//        removeAll();
+//        for (let i = 0; i < cities.length; i++) {
+//            citiesSel.options[citiesSel.options.length] = new Option(cities[1][i], cities[1][i]);
+//        }
+//    } else if (e.target.value == "Ukraine"){
+//        removeAll();
+//        for (let i = 0; i < cities.length; i++) {
+//            citiesSel.options[citiesSel.options.length] = new Option(cities[2][i], cities[2][i]);
+//        }
+//    }
+// })
+
+// citiesSel.addEventListener("change", e=>{
+//    p.innerHTML += ", " + e.target.value;
+// })
+
+// function removeAll() {
+//    while (citiesSel.options.length > 0) {
+//        citiesSel.remove(0);
+//    }
 // }
 
-//     function changecat(value) {
-//         if (value.length == 0) document.querySelector("#country").innerHTML = "<option></option>";
-//         else {
-//             var catOptions = "";
-//             for (categoryId in mealsByCategory[value]) {
-//                 catOptions += "<option>" + mealsByCategory[value][categoryId] + "</option>";
-//             }
-//             document.querySelector("#cities").innerHTML = catOptions;
-//         }
-//     }
+const citiesOption = {
+   ukr: ['Kyiv', 'Lviv', 'Dnipro', 'Odessa'],
+   usa: ['New-York', 'Washington', 'Boston', 'Chikago'],
+   ger: ['Hamburg', 'Dresden', 'Dusseldorf', 'Berlin']
+};
+
+let countrySel = document.querySelector("#country");
+countrySel.addEventListener("change", createList);
+countrySel.addEventListener("change", toParagraph);
+
+let citySel = document.querySelector("#cities");
+citySel.addEventListener("change", toParagraph);
+
+function createList() {
+   citySel.innerHTML = "";
+   let selectedCountry = countrySel.value;
+   for(let i = 0; i < citiesOption[selectedCountry].length; i++) {
+      let newOption = document.createElement("option");
+      newOption.innerHTML = citiesOption[selectedCountry][i];
+      citySel.appendChild(newOption);
+   }
+};
+
+let paragraph = document.querySelector("p");
+function toParagraph() {
+   paragraph.innerHTML = "";
+   paragraph.innerHTML = countrySel.options[countrySel.selectedIndex].text + ", " + citySel.options[citySel.selectedIndex].text;
+};
+
